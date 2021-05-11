46e4c0dea25211e1e4852763941d1247b4fe04b0
EloxZ <45355879+EloxZ@users.noreply.github.com>
2021-05-11 18:08:40 +0200

    Cambios

---
+++ src/java/app/dao/RolesFacade.java
@@ -0,0 +1,32 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package app.dao;
+
+import app.entity.Roles;
+import javax.ejb.Stateless;
+import javax.persistence.EntityManager;
+import javax.persistence.PersistenceContext;
+
+/**
+ *
+ * @author EloyB
+ */
+@Stateless
+public class RolesFacade extends AbstractFacade<Roles> {
+
+    @PersistenceContext(unitName = "EvenTAWPU")
+    private EntityManager em;
+
+    @Override
+    protected EntityManager getEntityManager() {
+        return em;
+    }
+
+    public RolesFacade() {
+        super(Roles.class);
+    }
+    
+}
---
+++ src/java/app/entity/Estudio.java
@@ -0,0 +1,79 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package app.entity;
+
+import java.io.Serializable;
+import javax.persistence.Basic;
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.NamedQueries;
+import javax.persistence.NamedQuery;
+import javax.persistence.Table;
+import javax.xml.bind.annotation.XmlRootElement;
+
+/**
+ *
+ * @author EloyB
+ */
+@Entity
+@Table(name = "ESTUDIO")
+@XmlRootElement
+@NamedQueries({
+    @NamedQuery(name = "Estudio.findAll", query = "SELECT e FROM Estudio e")
+    , @NamedQuery(name = "Estudio.findByIdEstudio", query = "SELECT e FROM Estudio e WHERE e.idEstudio = :idEstudio")})
+public class Estudio implements Serializable {
+
+    private static final long serialVersionUID = 1L;
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Basic(optional = false)
+    @Column(name = "ID_ESTUDIO")
+    private Integer idEstudio;
+
+    public Estudio() {
+    }
+
+    public Estudio(Integer idEstudio) {
+        this.idEstudio = idEstudio;
+    }
+
+    public Integer getIdEstudio() {
+        return idEstudio;
+    }
+
+    public void setIdEstudio(Integer idEstudio) {
+        this.idEstudio = idEstudio;
+    }
+
+    @Override
+    public int hashCode() {
+        int hash = 0;
+        hash += (idEstudio != null ? idEstudio.hashCode() : 0);
+        return hash;
+    }
+
+    @Override
+    public boolean equals(Object object) {
+        // TODO: Warning - this method won't work in the case the id fields are not set
+        if (!(object instanceof Estudio)) {
+            return false;
+        }
+        Estudio other = (Estudio) object;
+        if ((this.idEstudio == null && other.idEstudio != null) || (this.idEstudio != null && !this.idEstudio.equals(other.idEstudio))) {
+            return false;
+        }
+        return true;
+    }
+
+    @Override
+    public String toString() {
+        return "app.entity.Estudio[ idEstudio=" + idEstudio + " ]";
+    }
+    
+}
