da458079d1891ff6dc3dc1b3fd38aa3902e5a63f
AaronHDN <aaronmape@hotmail.com>
2021-05-11 17:43:25 +0200

    Projecto bueno

---
+++ src/java/eventos/dao/ConversacionFacade.java
@@ -0,0 +1,32 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package eventos.dao;
+
+import eventos.entity.Conversacion;
+import javax.ejb.Stateless;
+import javax.persistence.EntityManager;
+import javax.persistence.PersistenceContext;
+
+/**
+ *
+ * @author aaron
+ */
+@Stateless
+public class ConversacionFacade extends AbstractFacade<Conversacion> {
+
+    @PersistenceContext(unitName = "EventosPU")
+    private EntityManager em;
+
+    @Override
+    protected EntityManager getEntityManager() {
+        return em;
+    }
+
+    public ConversacionFacade() {
+        super(Conversacion.class);
+    }
+    
+}
---
+++ src/java/eventos/dao/EventoFacade.java
@@ -0,0 +1,32 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package eventos.dao;
+
+import eventos.entity.Evento;
+import javax.ejb.Stateless;
+import javax.persistence.EntityManager;
+import javax.persistence.PersistenceContext;
+
+/**
+ *
+ * @author aaron
+ */
+@Stateless
+public class EventoFacade extends AbstractFacade<Evento> {
+
+    @PersistenceContext(unitName = "EventosPU")
+    private EntityManager em;
+
+    @Override
+    protected EntityManager getEntityManager() {
+        return em;
+    }
+
+    public EventoFacade() {
+        super(Evento.class);
+    }
+    
+}
